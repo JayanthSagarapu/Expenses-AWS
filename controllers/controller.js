3a40bc4739e8ff6a18a8adaae3cb799bda3a36bc
JayanthSagarapu <sjayanthkumar1@gmail.com>
2023-06-08 15:11:06 +0530

    encrypted password with bcrypt

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,29 +1,48 @@
 const User = require("../models/UserDb");
+const bcrypt = require("bcrypt");
 
 exports.addUser = async (req, res, next) => {
-  const { username, email, password } = req.body;
+  try {
+    const { username, email, password } = req.body;
 
-  const user = await User.create({
-    username: username,
-    email: email,
-    password: password,
-  });
-  console.log(user);
-  res.send(user);
+    const saltrounds = 10;
+    bcrypt.hash(password, saltrounds, async (err, hash) => {
+      const user = await User.create({
+        username: username,
+        email: email,
+        password: hash,
+      });
+      res.status(200).json({
+        message: "Successfully Created new User",
+      });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 exports.loginUser = async (req, res) => {
-  const { email, password } = req.body;
-  const finduser = await User.findOne({ where: { email } });
-  if (finduser) {
-    if (password == finduser.password) {
-      res.status(200).json({
-        message: "Successfully Logged In",
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (user) {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+          res
+            .status(200)
+            .json({ success: true, message: "Successfully Logged In" });
+        } else {
+          res
+            .status(401)
+            .json({ success: false, message: "Password is wrong" });
+        }
       });
     } else {
-      res.status(403).json({ message: "Password is wrong" });
+      res
+        .status(404)
+        .json({ success: false, message: "User not found in table" });
     }
-  } else {
-    res.status(404).json({ message: "User not found in table" });
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
